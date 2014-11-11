712de4f74e0364e8c23cfe555b2c724788681c2a
Amin Sojoudi <masojoudi@gmail.com>
2014-11-11 10:23:02 +0330

    Initial Commit

---
+++ BaseOpenCVProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BaseOpenCVProject
+//
+//  Created by Amin Sojoudi on 11/11/14.
+//  Copyright (c) 2014 Amin Sojoudi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
